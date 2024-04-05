3453db34dd4c9a027eb96bc0e00aa5341ce7c58d
Michael Czechowski <m.czechowski@pons.de>
2024-04-05 14:17:06 +0200

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count * count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
